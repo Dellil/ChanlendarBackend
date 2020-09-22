a70dac80981b12a7ae973e77addf72eb859a7b67
Dellil <aksaksdm@naver.com>
2020-09-22 16:02:57 +0900

    add POST /jwt/tokens

--- routes/jwt.js
+++ routes/jwt.js
@@ -14,4 +14,21 @@ router.post("/accessToken", (req, res, next) => {
 	});
 });
 
+router.post("/tokens", (req, res, next) => {
+	const existedRefreshToken = req.body.refreshToken;
+	jwt.verify(existedRefreshToken, process.env.JWT_REFRESH, (err, decoded) => {
+		if (err) {
+			return res.status(404).json({ message: err.message });
+		}
+		const accessToken = jwt.sign({ decoded }, process.env.JWT_ACCESS, {
+			expiresIn: "1day",
+		});
+		const refreshToken = jwt.sign({ decoded }, process.env.JWT_REFRESH, {
+			expiresIn: "30days",
+		});
+
+		res.status(201).json({ accessToken, refreshToken });
+	});
+});
+
 module.exports = router;
